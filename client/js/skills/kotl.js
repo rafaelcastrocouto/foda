72c64e282455315ec1b8e88023f27b57633b0c85
@racascou <rafaelcastrocouto@gmail.com>
2018-12-03 12:58:46 -0200

    0.4.61 - Kotl update 720

--- client/js/skills/kotl.js
+++ client/js/skills/kotl.js
@@ -5,7 +5,7 @@ game.skills.kotl = {
       var direction = kotl.getDirectionStr(target);
       var side = kotl.side();
       skill.addClass('channel-on');
-      kotl.selfBuff(skill, 'illuminate-channel');
+      kotl.selfBuff(skill, 'illuminate-channel');/*
       if (kotl.hasBuff('kotl-ult')) {
         var ghost = kotl.clone().removeClass('selected heroes can-attack can-move');
         ghost.addClass('illuminate-ghost ghost channeling');
@@ -15,7 +15,7 @@ game.skills.kotl = {
         ghost.data('illuminate-source', kotl);
         ghost.data('channeling', skill.data('channel'));
         source = ghost;
-      }
+      }*/
       source.addClass('illuminating illumi-'+direction);
       source.data('illuminate', skill);
       source.data('illuminate-target', target);
@@ -38,7 +38,7 @@ game.skills.kotl = {
       source.opponentsInLine(target, range, width, function (card) {
         kotl.damage(damage * time, card, skill.data('damage type'));
       });
-      if (source.hasClass('ghost')) {
+      if (!game.camera.hasClass('night')) {
         source.alliesInLine(target, range, width, function (card) {
           card.heal(damage * time);
         });
@@ -46,116 +46,11 @@ game.skills.kotl = {
       game.audio.stop('kotl/illuminate');
       game.audio.play('kotl/illuminaterelease');
       kotl.data('illuminate-target', null);
-      kotl.data('illuminate-ghost', null);
       kotl.off('turnend.kotl-illuminate');
       kotl.removeBuff('kotl-illuminate');
       kotl.removeClass('illuminating illumi-left illumi-right illumi-top illumi-bottom');
-      if (source.hasClass('ghost')) source.discard();
       skill.data('discard-to', false);
       skill.removeClass('channel-on').discard();
-    }
-  },
-  leak: {
-    cast: function (skill, source, target) {
-      game.fx.add('kotl-leak', source, target);
-      var buff = source.addBuff(target, skill);
-      buff.on('expire', this.expire);
-      target.shake();
-      target.data('kotl-leak', skill);
-      target.on('moved.kotl-leak', this.moved);
-      game.skills.kotl.leak.discard(target, source, skill);
-    },
-    moved: function (event, eventdata) {
-      var target = eventdata.card;
-      target.shake();
-      if (target.hasBuff('kotl-leak')) {
-        var buff = target.getBuff('kotl-leak');
-        var source = buff.data('source');
-        var skill = target.data('kotl-leak');
-        game.skills.kotl.leak.discard(target, source, skill);
-      } else {
-        target.off('moved.kotl-leak');
-        game.fx.stop('kotl-leak', target);
-      }
-    },
-    discard: function (target, source, skill) {
-      var hero = target.data('hero');
-      var opponent = target.side();
-      var card = $('.'+opponent+' .hand .'+hero).randomCard();
-      if (card.length) card.discard();
-      else {
-        source.addStun(target, skill);
-        target.off('moved.kotl-leak');
-        game.fx.stop('kotl-leak', target);
-        target.removeBuff('kotl-leak');
-      }
-    },
-    expire: function (event, eventdata) {
-      var target = eventdata.target;
-      target.off('moved.kotl-leak');
-      game.fx.stop('kotl-leak', target);
-    }
-  },
-  mana: {
-    cast: function (skill, source) {
-      var side = source.side();
-      var bonus = skill.data('bonus cards');
-      game.skill.buyCards(bonus, side);
-      game.fx.add('kotl-mana', source);
-    }
-  },
-  ult: {
-    cast: function (skill, source) {
-      game.fx.add('kotl-ult', source);
-      var side = source.side();
-      if (!source.hasBuff('kotl-ult')) {
-        var illuminate = $('.table .'+side+' .skills .kotl-illuminate');
-        //skill.addClass('on');
-        source.on('turnend.kotl-ult', this.turnend);
-        source.addClass('kotl-ult');
-        source.data('kotl-ult', skill);
-        illuminate.data('type', game.data.ui.active);
-        illuminate.find('.type').text(game.data.ui.active);
-        illuminate.addClass('spiritform');
-      }
-      source.selfBuff(skill);
-      var recall = $('.table .'+side+' .skills .kotl-recall');
-      if (recall.parent()[0] !== game[side].skills.hand[0]) {
-        recall.appendTo(game[side].skills.hand).removeClass('casted');
-      }
-      var blind = $('.table .'+side+' .skills .kotl-blind');
-      if (blind.parent()[0] !== game[side].skills.hand[0]) {
-        blind.appendTo(game[side].skills.hand).removeClass('casted');
-      }
-    },
-    turnend: function (event, eventdata) {
-      var source = eventdata.target;
-      if (!source.hasBuff('kotl-ult')) {
-        game.skills.kotl.ult.off(source);
-      }
-    },
-    off: function (source, event) {
-      var side = source.side();
-      var recall = $('.table .'+side+' .skills .kotl-recall');
-      var blind = $('.table .'+side+' .skills .kotl-blind');
-      var illuminate = $('.table .'+side+' .skills .kotl-illuminate');
-      var skill = source.data('kotl-ult');
-      if (event) source.removeBuff('kotl-ult');
-      skill.removeClass('on');
-      illuminate.data('type', game.data.ui.channel);
-      illuminate.removeClass('spiritform');
-      var ghost = source.data('illuminate-ghost');
-      if (ghost) {
-        game.skills.kotl.illuminate.release(ghost.data('skill'), ghost);
-      }
-      source.off('turnend.kotl-ult');
-      source.removeClass('kotl-ult');
-      source.data('kotl-ult', null);
-      recall.discard();
-      blind.discard();
-    },
-    channelend: function (event, eventdata) {
-      this.off(eventdata.source, true);
     }
   },
   blind: {
@@ -193,43 +88,43 @@ game.skills.kotl = {
       }
     }
   },
-  recall: {
+  mana: {
+    cast: function (skill, source) {
+      var side = source.side();
+      var bonus = skill.data('bonus cards');
+      game.skill.buyCards(bonus, side);
+      game.fx.add('kotl-mana', source);
+    }
+  },
+  ult: {
     cast: function (skill, source, target) {
-      game.fx.add('kotl-recall-source', source);
-      game.fx.add('kotl-recall', source, target);
-      var buff = source.addBuff(target, skill);
-      buff.on('expire.kotl-recall', this.expire);
-      target.on('damage.kotl-recall', this.damage);
-      target.data('recall-source', source);
-      target.data('recall-skill', skill);
-    },
-    damage: function (event, eventdata) {
-      var target = eventdata.target;
-      var source = target.data('recall-source');
-      target.removeBuff('kotl-recall');
-      target.off('damage.kotl-recall');
-      game.fx.stop('kotl-recall', target);
-      game.fx.stop('kotl-recall-source', source);
-      target.data('recall-source', null);
-      target.data('recall-skill', null);
-    },
-    expire: function (event, eventdata) {
-      var target = eventdata.target;
-      var source = target.data('recall-source');
-      var destiny;
-      source.around(game.data.ui.melee, function (spot) {
-        if (spot.hasClass('free') && target.data('recall-skill')) destiny = spot;
+      var kotl = source;
+      game.fx.add('kotl-ult', source, target);
+      game.timeout(800, function () {
+        var side = source.side();
+        var opponent = game.opponent(side);
+        var card = $('.card', target);
+        var range = skill.data('aoe range');
+        if (kotl.data('skill range bonus')) range += kotl.data('skill range bonus');
+        if (card.hasClass(opponent)) game.skills.kotl.ult.target(skill, source, card);
+        target.inRange(range, function (spot) {
+          var card = $('.card.'+opponent, spot);
+          if (card.length && !card.hasClasses('tower ghost bkb')) {
+            game.skills.kotl.ult.target(skill, source, card);
+          }
+        });
       });
-      if (destiny && !target.hasClasses('bkb cycloned')) {
-        target.stopChanneling();
-        target.place(destiny);
-        game.audio.play('kotl/recallend');
-        target.data('recall-source', null);
-        target.data('recall-skill', null);
-        target.off('damage.kotl-recall');
-        game.fx.stop('kotl-recall', target);
-        game.fx.stop('kotl-recall-source', source);
-      }
+    },
+    target: function (skill, source, target) {
+      source.addBuff(target, skill);
+      target.addClass('sleeping');
+      target.on('attacked.kotl-ult', this.attacked);
+      target.stopChanneling();
+    },
+    attacked: function (event, eventdata) {
+      var target = eventdata.target;
+      target.removeBuff('kotl-ult');
+      target.removeClass('sleeping');
     }
   }
 };
