889eb768cc47146990cb40a171f0720a9a1e80d3
@racascou <rafaelcastrocouto@gmail.com>
2020-09-24 20:08:01 -0300

    0.5.56 - Fix dotenv on production

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 var nodeEnv = process.env.NODE_ENV || 'development'; 
 var host = process.env.HOST;
